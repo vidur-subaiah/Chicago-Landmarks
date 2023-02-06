c238ab737530367cbdf94f6c9523091e92b55098
vidur-subaiah <47302924+vidur-subaiah@users.noreply.github.com>
2023-02-05 19:42:05 -0600

    Add initial storyboard

---
+++ My-Kind-Of-Town/My-Kind-Of-Town/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  My-Kind-Of-Town
+//
+//  Created by Vidur Subaiah on 2/5/23.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.mapView.showsCompass = false
+        self.mapView.pointOfInterestFilter = .excludingAll
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ My-Kind-Of-Town/My-Kind-Of-Town/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-Kind-Of-Town
+//
+//  Created by Vidur Subaiah on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
